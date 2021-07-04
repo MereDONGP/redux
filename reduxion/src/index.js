fb3109c253aa078543bc5ef1e604ac8a36f557ee
Don <pullend15@gmail.com>
2021-07-03 21:25:54 -0400

    made the button to count

--- reduxion/src/index.js
+++ reduxion/src/index.js
@@ -4,20 +4,39 @@ import {createStore} from 'redux';
 
 const incrementing = (state = 0, action) => {
   switch(action.type){
+    case 'increase':
+      return state + 1
     
+    case 'decrease':
+      return state - 1
+    
+    case 'zero':
+      return 0
+    
+    default:
+      return state 
   }
 }
 
+const store = createStore(incrementing)
+
 function App() {
-  
   return (
-    <h1>Hello world</h1>
+    <div>
+    <h1>{store.getState()}</h1>
+    <button onClick={() => store.dispatch({type:'increase'})}>increase</button>
+    <button onClick={() => store.dispatch({type:'decrease'})}>decrease</button>
+    <button onClick={() => store.dispatch({type:'zero'})}>zero</button>
+    </div>
   )
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const renderapp = () => { 
+  ReactDOM.render(
+    <App />,
   document.getElementById('root')
-);
+  )
+};
+
+renderapp()
+store.subscribe(renderapp)
