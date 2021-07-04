98a9dbea882cec2614b6f0407aad0ae0cbbc2442
Don <pullend15@gmail.com>
2021-07-03 22:22:56 -0400

    adding in the functional things

--- reduxion/src/index.js
+++ reduxion/src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 
-const incrementing = (state = 0, action) => {
+const initial = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const incrementing = (state = initial, action) => {
   switch(action.type){
-    case 'increase':
-      return state + 1
+    case 'good':
+      return state = {...state, "good": state.good + 1}
     
-    case 'decrease':
-      return state - 1
+    case 'bad':
+      return state = {...state, "bad": state.bad + 1}
+    
+    case 'ok':
+      return state = {...state, "ok": state.ok + 1}
     
     case 'zero':
-      return 0
+      return state = {'good': 0, 'ok': 0, 'bad': 0}
     
     default:
       return state 
@@ -23,9 +32,12 @@ const store = createStore(incrementing)
 function App() {
   return (
     <div>
-    <h1>{store.getState()}</h1>
-    <button onClick={() => store.dispatch({type:'increase'})}>increase</button>
-    <button onClick={() => store.dispatch({type:'decrease'})}>decrease</button>
+      <div>Good: {store.getState().good}</div>
+      <div>Ok: {store.getState().ok}</div>
+      <div>Bad: {store.getState().bad}</div>
+    <button onClick={() => store.dispatch({type:'good'})}>good</button>
+    <button onClick={() => store.dispatch({type:'ok'})}>ok</button>
+    <button onClick={() => store.dispatch({type:'bad'})}>bad</button>
     <button onClick={() => store.dispatch({type:'zero'})}>zero</button>
     </div>
   )
